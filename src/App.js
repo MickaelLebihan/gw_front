acee080e21e38223874b814655184f60b7d23b5a
mickaellebihan <lebihan.mickael@gmail.com>
2023-05-17 15:26:15 +0200

    routes working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import './styles/index.scss';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './routes/Home'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="test"></div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/Home/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './index.scss'
+
+export default function index() {
+  return (
+    <main>
+        <div className="newses">
+            <h2>news</h2>
+            <div className="news">
+              <h3>titre 1</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est autem mollitia delectus obcaecati ex placeat voluptatum modi ipsum, magni id. Consequuntur, dolorem voluptate. Veritatis necessitatibus rerum nesciunt laudantium corrupti sint?</p>
+            </div>
+            <div className="news">
+              <h3>titre 2</h3>
+              <p>Odit eius consectetur maxime, aliquam id omnis quae quaerat beatae optio architecto amet similique, totam temporibus esse voluptates accusamus tempore ipsam! Ratione, vero? Vero voluptates pariatur fuga praesentium officia sunt.</p>
+            </div>
+            <div className="news">
+              <h3>titre 3</h3>
+              <p>Iusto quam commodi mollitia maxime delectus, ab minima. Tempora consequuntur laboriosam cumque. Explicabo tempore dolores sunt voluptatem quae ad nesciunt aliquam molestiae, ipsum harum, ullam illum vitae eligendi odit quibusdam!</p>
+            </div>
+            <div className="news">
+              <h3>titre 4</h3>
+              <p>Deleniti autem iste excepturi perspiciatis ipsum quod esse, nihil ex unde recusandae consectetur eos iusto accusantium vitae atque architecto hic dignissimos optio ea dolorem? Quos neque molestiae atque autem blanditiis.</p>
+            </div>
+            <div className="news">
+              <h3>titre 5</h3>
+              <p>Recusandae non animi molestiae a iure ipsum quidem doloremque eius? Eveniet, architecto maxime. Asperiores consequuntur odio officia delectus harum aliquid dolore nemo necessitatibus ad minima, fugiat nobis ratione nulla tenetur?</p>
+            </div>
+        </div>
+        <div className="content">
+          <h3>contenu principal</h3>
+        </div>
+    </main>
+  )
+}
